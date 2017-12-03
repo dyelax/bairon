16bba62f36d3e5389e22584983be992133a643dd
Martha Edwards <martha_edwards@brown.edu>
2017-12-02 20:14:57 -0500

    Update server for consistent json

--- web/flask-backend/server.py
+++ web/flask-backend/server.py
@@ -26,31 +26,26 @@ def everything():
 
   return json.dumps(results)
 
-@app.route('/bairon/<poem>', methods=['GET'])
 def bairon(poem):
   if not poem:
     return "Not implemented yet! (random seed)"
   return "Not implemented yet! (seed provided)"
 
-@app.route('/thesaurus/poem/<poem>', methods=['GET'])
 def thesaurus(poem):
-  poem = poem.split()
-  pattern = re.compile('[\W_]+')
-  for word in poem:
-    word = pattern.sub('', word)
-  print(poem)
+  poem = split_poem_into_words(poem)
   length = len(poem)
 
   tries = 0
   while (tries < length):
     i = random.randint(0, length - 1)
     if len(poem[i]) > 3:
-      results = thesaurus_word(poem[i])
-      if len(results) > 2:
-        return json.dumps(results)
+      words = thesaurus_word(poem[i])
+      if len(words) > 2:
+        results = {}
+        results[poem[i]] = words
+        return results
   return []
 
-@app.route('/thesaurus/word/<word>', methods=['GET'])
 def thesaurus_word(word):
   url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' + \
     word.lower() + '/synonyms'
@@ -66,12 +61,29 @@ def thesaurus_word(word):
   synonyms = r.json()['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['synonyms'][:5]
   synonyms = list(map(lambda x: x['text'], synonyms))
 
-  return json.dumps(synonyms)
+  return synonyms
 
-@app.route('/rhyme/poem/<poem>', methods=['GET'])
 def rhyme(poem):
-  return 'Not implemented yet'
+  poem = split_poem_into_words(poem)
+  length = len(poem)
 
-@app.route('/rhyme/word/<word>', methods=['GET'])
+  tries = 0
+  while (tries < length):
+    i = random.randint(0, length - 1)
+    if len(poem[i]) > 3:
+      words = rhyme_word(poem[i])
+      if len(words) > 2:
+        results = {}
+        results[poem[i]] = words
+        return results
+  return []
+
 def rhyme_word(word):
   return 'Not implemented yet'
+
+def split_poem_into_words(poem):
+  poem = poem.split()
+  pattern = re.compile('[\W_]+')
+  for word in poem:
+    word = pattern.sub('', word)
+  return poem
